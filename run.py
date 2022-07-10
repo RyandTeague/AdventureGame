55e7671ac98f6b2a1c77b9f2001530022f0d4bd2
RyandTeague <ryanteague@hotmail.co.uk>
2022-07-10 14:26:12 +0000

    creat item class and gold subclass

--- run.py
+++ run.py
@@ -1,4 +1,25 @@
 from adventurelib import *
 
+# Creating object classes
+
+class Item():
+    """The base class for all items"""
+    def __init__(self, name, description, value):
+        self.name = name
+        self.description = description
+        self.value = value
+ 
+    def __str__(self):
+        return "{}\n=====\n{}\nValue: {}\n".format(self.name, self.description, self.value
+
+# Subclasses of Item
+
+class Gold(Item):
+    def __init__(self, amount):
+        self.amount = amount
+        super().__init__(name="Gold",
+                         description="A round coin with {} stamped on the front.".format(str(self.amt)),
+                         value=self.amount)
+
 
 start()
