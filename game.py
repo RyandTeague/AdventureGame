1f9dfe2ba78d0cfc090136a3a53c8007e314b2ca
RyandTeague <ryanteague@hotmail.co.uk>
2022-07-12 11:20:05 +0000

    add message when false input

--- game.py
+++ game.py
@@ -22,6 +22,8 @@ def play():
                 if action_input == action.hotkey:
                     player.do_action(action, **action.kwargs)
                     break
+                else:
+                    print("Not an available action, try again")
 
 
 if __name__ == "__main__":
